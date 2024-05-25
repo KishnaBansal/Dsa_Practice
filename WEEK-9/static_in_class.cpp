33c63ae0a7ecc1d27687c467b5c2b96d6d764385
Kishna Bansal <kb.20051981@gmail.com>
2024-05-25 23:28:27 +0530

    local,global,const ,static,macros concepts

---
+++ WEEK-9/static_in_class.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+using namespace std;
+
+class abc {
+    public:
+        static int x,y;
+        void print() const {
+            // cout<<x<<" "<<y<<endl;
+
+            //this  (hidden parameter)
+            cout<<this->x<<" "<<this->y<<endl;
+        }
+};
+
+int abc::x;
+int abc::y;
+
+int main() {
+    abc obj1;
+    obj1.x = 1;
+    obj1.y = 2;
+    obj1.print();
+    abc obj2;
+    obj2.x = 3;
+    obj2.y = 4;
+    obj1.print();
+    obj2.print();
+}
