7ed500c842b4547db98a8611245d7a07d8a9cb1d
Kishna Bansal <kb.20051981@gmail.com>
2024-09-27 22:52:28 +0530

    learned new concept of Sliding Window

--- WEEK-12/Queues_2.cpp
+++ WEEK-12/Queues_2.cpp
@@ -94,43 +94,91 @@ void interleaveQueue(queue<int>&q) {
     }
 }
 
+// void firstNegativeInt(int *arr,int n,int k) {
+//     deque<int>dq;
+
+//     //process first k elements - first window
+//     for(int i=0;i<k;i++) {
+//         int temp = arr[i];
+//         if(temp < 0) {
+//             dq.push_back(i);
+//         }
+//     }
+
+//     //process reamining windows - removal and addition
+//     //Remaining elememnts will start from index k
+//     for(int i=k;i<n;i++) {
+//         //but first find old window answer
+//         if(dq.empty()) {
+//         cout<<"0"<<endl;
+//         }
+//         else {
+//             cout<<arr[dq.front()];
+//         }
+//         //Removal - jo bhi index out of range h use queue se remove kardo
+//         if(i - dq.front() >= k) {
+//             dq.pop_front();
+//         }
+//         //Addition
+//         if(arr[i] < 0) {
+//             dq.push_back(i); 
+//         }
+//     }
+
+//     //Last window
+//     if(dq.empty()) {
+//         cout<<"0"<<endl;
+//     }
+//     else {
+//         cout<<arr[dq.front()];
+//     }
+// }
+
 void firstNegativeInt(int *arr,int n,int k) {
-    deque<int>dq;
+    deque<int> dq;
 
     //process first k elements - first window
     for(int i=0;i<k;i++) {
-        int temp = arr[i];
-        if(temp < 0) {
+        int element = arr[i];
+        if(element < 0) {
             dq.push_back(i);
         }
     }
+    // //print first answer
+    // if(dq.empty()) {
+    //     cout<<"0"<<" ";
+    // }
+    // else {
+    //     cout<<arr[dq.front()]<<" ";
+    // }
 
-    //process reamining windows - removal and addition
-    //Remaining elememnts will start from index k
+    //process remaining window - removal and addition
+    //Previous loop ends at (k-1)th index. So the next loop will start from k index 
     for(int i=k;i<n;i++) {
-        //but first find old window answer
+        //aage badhne se pehle print -ve elements of old window
         if(dq.empty()) {
-        cout<<"0"<<endl;
+        cout<<0<<" ";
         }
         else {
-            cout<<arr[dq.front()];
+            cout<<arr[dq.front()]<<" ";
         }
-        //Removal - jo bhi index out of range h use queue se remove kardo
+        //Removal
+        //Jo bhi index out of range hai use queue se remove kardo
         if(i - dq.front() >= k) {
             dq.pop_front();
         }
         //Addition
         if(arr[i] < 0) {
-            dq.push_back(i); 
+            dq.push_back(i);
         }
     }
 
     //Last window
     if(dq.empty()) {
-        cout<<"0"<<endl;
+        cout<<0<<" ";
     }
     else {
-        cout<<arr[dq.front()];
+        cout<<arr[dq.front()]<<" ";
     }
 }
 
@@ -141,11 +189,15 @@ int main() {
     // q.push(30);
     // q.push(40);
     // q.push(50);
-    int n = q.size();
-    int k = 3;
+    // int n = q.size();
+    // int k = 3;
     // reverseQueue(q);   
     // reverseQueueRec(q); 
     // reverseFirstK(q,n,k);
-    interleaveQueue(q);
+    // interleaveQueue(q);
+    int arr[] = {-8,2,3,-6,10};
+    int n = sizeof(arr)/sizeof(arr[0]);
+    int k = 2;
+    firstNegativeInt(arr,n,k);
     cout<<endl;
 }
---
+++ WEEK-12/doubt_1.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+#include<queue>
+#include<stack>
+#include<deque>
+using namespace std;
+
+int main() {
+    
+}
